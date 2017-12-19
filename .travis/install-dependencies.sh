625b9a9b72c218a622fce24c47847eb27a01d2f0
Bernie Pope <bjpope@unimelb.edu.au>
2017-12-19 14:52:41 +1100

    Try to use cpanm to install dependencies

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -4,7 +4,5 @@
 
 echo 'Perl5 install'
 (
-   sudo apt-get update
-   sudo apt-get install -y bioperl liblog-log4perl-perl cpanminus libreadonly-perl
-   sudo cpanm Getopt::ArgParse
+    cpanm --quiet --installdeps --notest . 
 )
---
+++ cpanfile
@@ -0,0 +1,5 @@
+requires 'Bio'
+requires 'Log::Log4perl'
+requires 'Getopt::ArgParse'
+requires 'Try::Tiny'
+requires 'Readonly'
