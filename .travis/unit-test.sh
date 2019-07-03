02f88d28312b9f6861788fc3d23ffcd30bb606ac
Bernie Pope <bjpope@unimelb.edu.au>
2019-07-03 14:17:22 +1000

    Remove sub-shell

--- .travis/unit-test.sh
+++ .travis/unit-test.sh
@@ -4,12 +4,10 @@ set -e
 errors=0
 
 # Run unit tests
-(
-  ./bionitio_test.pl || {
+./bionitio_test.pl || {
     echo "'bionitio_test.pl' failed"
     let errors+=1
   }
-)
 
 [ "$errors" -gt 0 ] && {
     echo "There were $errors errors found"
